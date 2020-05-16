018436e856bee2edfc28c8b26bae2e14f0e8e886
Taryn Martin <taryn.martin919@gmail.com>
2020-05-16 11:40:38 -0600

    Change: add toggle function to switch button view when changing page view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // Create variables targetting the relevant DOM elements here 👇
-var currentCover = document.querySelector('.cover-image');
+var randCover = document.querySelector('.cover-image');
 var currentTitle = document.querySelector('.cover-title');
 var currentDesc1 = document.querySelector('.tagline-1');
 var currentDesc2 = document.querySelector('.tagline-2');
 var randomCoverButton = document.querySelector('.random-cover-button');
+var makeOwnCoverButton = document.querySelector('.make-new-button')
+var homeButton = document.querySelector('.home-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -13,6 +16,7 @@ var currentCover;
 
 // Add your event listeners here 👇
 randomCoverButton.addEventListener('click', randomCover);
+makeOwnCoverButton.addEventListener('click', toggleMode);
 
 // Create your event handlers and other functions here 👇
 window.onload = randomCover;
@@ -25,28 +29,16 @@ function getRandomIndex(array) {
 
 //Iteration 0
 function randomCover() {
-  var randomNumCover = getRandomIndex(covers);
-  var randomNumTitle = getRandomIndex(titles);
-  var randomNumDesc1 = getRandomIndex(descriptors);
-  var randomNumDesc2 = getRandomIndex(descriptors);
-  currentCover.src = covers[randomNumCover];
-  currentTitle.innerText = titles[randomNumTitle];
-  currentDesc1.innerText = descriptors[randomNumDesc1];
-  currentDesc2.innerText = descriptors[randomNumDesc2];
+  currentCover = new Cover (randCover.src = covers[getRandomIndex(covers)], currentTitle.innerText = titles[getRandomIndex(titles)], currentDesc1.innerText = descriptors[getRandomIndex(descriptors)], currentDesc2.innerText = descriptors[getRandomIndex(descriptors)]);
 }
 
-
-//Iteration 1
-//Every time user clicks "Show random cover" button, new random cover is created
-//Event listener: click (calls random cover generator)
-//Event handler: the randomCover functions
-//Display object: function to display info on the dom - push new random cover to the DOM tree replacing current class cover with new random variable values
-
-//Iteration 2
-//When a user clicks the make your own cover button, we should see form & home page view should be hidden
-//Event listener: user click "make your own cover"
-//Event handler: new function that changes html class to remove hidden for the form page
-//Event handler: function that changes html class to hide the home page; save cover button hidden, new random cover button should be hidden; home button should be visible
+function toggleMode() {
+  document.querySelector('.home-view').classList.toggle('hidden');
+  document.querySelector('.form-view').classList.toggle('hidden');
+  homeButton.classList.toggle('hidden');
+  saveCoverButton.classList.toggle('hidden');
+  randomCoverButton.classList.toggle('hidden');
+}
 
 //Iteration 3
 //In the form field, user should fill out 4 input fields & hit save button
