83d2ee1e4585a9c5f42b9fa219b83dc5edbca332
Taryn Martin <taryn.martin919@gmail.com>
2020-05-16 14:17:05 -0600

    Fix: adjusted toggle functions and reorganized variables

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // Create variables targetting the relevant DOM elements here 👇
-
 var randCover = document.querySelector('.cover-image');
 var currentTitle = document.querySelector('.cover-title');
 var currentDesc1 = document.querySelector('.tagline-1');
@@ -9,6 +8,9 @@ var makeOwnCoverButton = document.querySelector('.make-new-button')
 var homeButton = document.querySelector('.home-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var savedCoversButton = document.querySelector('.view-saved-button');
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
+var savedView = document.querySelector('.saved-view');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -17,16 +19,11 @@ var savedCovers = [
 
 var currentCover;
 
-var randCover = document.querySelector('.cover-image');
-var currentTitle = document.querySelector('.cover-title');
-var currentDesc1 = document.querySelector('.tagline-1');
-var currentDesc2 = document.querySelector('.tagline-2');
-var randomCoverButton = document.querySelector('.random-cover-button');
-
 // Add your event listeners here 👇
 randomCoverButton.addEventListener('click', randomCover);
-makeOwnCoverButton.addEventListener('click', toggleMode);
+makeOwnCoverButton.addEventListener('click', toggleMakeCover);
 savedCoversButton.addEventListener('click', toggleSavedCovers);
+homeButton.addEventListener('click', toggleHome);
 
 // Create your event handlers and other functions here 👇
 window.onload = randomCover;
@@ -36,18 +33,20 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-//Iteration 0
 function randomCover() {
   currentCover = new Cover (randCover.src = covers[getRandomIndex(covers)], currentTitle.innerText = titles[getRandomIndex(titles)], currentDesc1.innerText = descriptors[getRandomIndex(descriptors)], currentDesc2.innerText = descriptors[getRandomIndex(descriptors)]);
 }
 
-function toggleMode() {
+function toggleMakeCover() {
   document.querySelector('.home-view').classList.toggle('hidden');
   document.querySelector('.form-view').classList.toggle('hidden');
   homeButton.classList.toggle('hidden');
   saveCoverButton.classList.toggle('hidden');
   randomCoverButton.classList.toggle('hidden');
+  if (savedCoversButton.clicked === true) {
+    toggleSavedCovers();
   }
+}
 
 function toggleSavedCovers() {
   document.querySelector('.home-view').classList.toggle('hidden');
@@ -55,15 +54,17 @@ function toggleSavedCovers() {
   homeButton.classList.toggle('hidden');
   randomCoverButton.classList.toggle('hidden');
   saveCoverButton.classList.toggle('hidden');
+  if (makeOwnCoverButton.clicked === true) {
+    toggleMakeCover();
+  }
 }
 
-
-
-
-
-//Iteration 2
-//on saved covers, hide save cover
-//
+function toggleHome() {
+  document.querySelector('.home-view').classList.toggle('hidden');
+  homeButton.classList.toggle('hidden');
+  randomCoverButton.classList.toggle('hidden');
+  saveCoverButton.classList.toggle('hidden');
+}
 
 //Iteration 3
 //In the form field, user should fill out 4 input fields & hit save button
